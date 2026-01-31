5baff669098b84cb2c225e87de09994e0583e049
yukioishi-eng <31rekichiq@yahoo.ne.jp>
2026-01-31 15:44:59 +0900

    日付つき学習ログを追記

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+"""
+学習ログ
+
+2026-01-31
+内容：
+・if文とfor文の復習
+・リストから条件に合う要素を取り出す練習
+"""
+
 scores = [78, 92, 85, 60, 88]
 
 passed = []
+
 for s in scores:
     if s >= 80:
         passed.append(s)
 
-print(passed)
-input("Enterで終了")
+print("80点以上:", passed)

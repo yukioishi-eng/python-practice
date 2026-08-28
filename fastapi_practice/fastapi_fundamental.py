7241ce7ecc84bc0d8c3e7efdad3df670f1e73939
yukioishi-eng <31rekichiq@yahoo.ne.jp>
2026-08-28 23:05:06 +0900

    docs: add code comments

--- fastapi_practice/fastapi_fundamental.py
+++ fastapi_practice/fastapi_fundamental.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+#@app.get()はFastAPIのルーティングデコレータで、HTTPのGETリクエストを受け取る
+#引数にはドメイン以下のパスを入れる
 @app.get("/")
 def read_root():
     return {"message": "APIです"}

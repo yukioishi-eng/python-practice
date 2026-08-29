503e025d9bcede9ef41822a2516f154fe543e1ef
yukioishi-eng <31rekichiq@yahoo.ne.jp>
2026-08-29 20:16:59 +0900

    feat: implement client.py for API interaction with example requests

---
+++ client.py
@@ -0,0 +1,16 @@
+import requests
+res = requests.get("http://127.0.0.1:8000/sample")
+print(res.status_code)
+print(res.text)    #{"message":"APIです"}
+#検索エンジンのアドレスバーにhttp://127.0.0.1:8000/docsやhttp://127.0.0.1:8000/openapi.jsonなどを入力すると、APIの詳細がわかる
+#自分が作ったAPIを外部と共有したいときなどに用いる
+
+res = requests.get("http://127.0.0.1:8000/items/110")
+print(res.status_code)
+print(res.text)    #{"items_id":"110","item_name":"Tシャツ"}
+#パスにあった結果が出力されている
+
+res = requests.get("http://127.0.0.1:8000/items/sample")
+print(res.status_code)
+print(res.text)    #{"items_id":"sample","item_name":"Tシャツ"}
+#read_itemとread_sample_itemで処理が競合するが、read_itemが上に書いてある方が優先される
